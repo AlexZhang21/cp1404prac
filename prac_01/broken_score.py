cc164b4320ccaef2497efb017ca80968d710baa1
AlexZhang21 <haolun.zhang@my.jc.edu.au>
2023-03-20 14:51:46 +0800

    12wecdds

---
+++ prac_01/broken_score.py
@@ -0,0 +1,13 @@
+
+
+
+        score = float(input("Enter score: "))
+        if score < 0 or score > 100:
+            print("Invalid score")
+        else:
+            if score > 50:
+                print("Passable")
+            if score > 90:
+                print("Excellent")
+            if score < 50:
+                print("Bad")
